d9455bdbc4153f8f4273f7deb2dd9d33a0555b31
kid <tianxiangkid@gmail.com>
2017-06-05 15:25:52 +0800

    尚未调通

---
+++ app/src/main/cpp/dcow.c.h
@@ -0,0 +1,8 @@
+//
+// Created by kid on 17-05-29.
+//
+
+#ifndef KIDDAYDREAM_DCOW_C_H
+#define KIDDAYDREAM_DCOW_C_H
+
+#endif //KIDDAYDREAM_DCOW_C_H
--- app/src/main/java/com/kid/daydream/SettingsActivity.kt
+++ app/src/main/java/com/kid/daydream/SettingsActivity.kt
@@ -12,11 +12,17 @@ class SettingsActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.setting_activity)
         initView();
+        Jni.dcow
     }
 
     private fun initView() {
         smb_address.setText("123")
         user_name.setText("123")
         password.setText("123")
+    }
+    companion object{
+        init{
+            System.loadLibrary("libsupportjni");   //defaultConfig.ndk.moduleName
+        }
     }
 }
